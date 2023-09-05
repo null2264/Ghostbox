a384f69e715f0223991301a180e63944fcda409d
ziro <palembani@gmail.com>
2023-09-05 11:08:19 +0700

    refactor: Patch goober

--- app/soapbox/containers/soapbox.tsx
+++ app/soapbox/containers/soapbox.tsx
@@ -170,11 +170,6 @@ const SoapboxMount = () => {
     }
   };
 
-  useEffect(() => {
-    const goober = document.querySelector('#_goober');
-    if (goober && !goober.getAttribute('nonce')) goober.setAttribute('nonce', window.__webpack_nonce__);
-  }, []);
-
   return (
     <ErrorBoundary>
       <BrowserRouter basename={BuildConfig.FE_SUBDIRECTORY}>
