7e1d4c51dde8d6e86c2e44e1d627f4dcabbe68bf
ziro <palembani@gmail.com>
2023-09-12 16:50:26 +0700

    chore: Reduce media limit to 20

--- app/soapbox/actions/timelines.ts
+++ app/soapbox/actions/timelines.ts
@@ -255,7 +255,7 @@ const expandAccountFeaturedTimeline = (accountId: string) =>
   expandTimeline(`account:${accountId}:pinned`, `/api/v1/accounts/${accountId}/statuses`, { pinned: true, with_muted: true });
 
 const expandAccountMediaTimeline = (accountId: string | number, { url, maxId }: Record<string, any> = {}) =>
-  expandTimeline(`account:${accountId}:media`, url || `/api/v1/accounts/${accountId}/statuses`, url ? {} : { max_id: maxId, only_media: true, limit: 40, with_muted: true });
+  expandTimeline(`account:${accountId}:media`, url || `/api/v1/accounts/${accountId}/statuses`, url ? {} : { max_id: maxId, only_media: true, limit: 20, with_muted: true });
 
 const expandListTimeline = (id: string, { url, maxId }: Record<string, any> = {}, done = noOp) =>
   expandTimeline(`list:${id}`, url || `/api/v1/timelines/list/${id}`, url ? {} : { max_id: maxId }, done);
@@ -270,7 +270,7 @@ const expandGroupTimelineFromTag = (id: string, tagName: string, { maxId }: Reco
   expandTimeline(`group:tags:${id}:${tagName}`, `/api/v1/timelines/group/${id}/tags/${tagName}`, { max_id: maxId }, done);
 
 const expandGroupMediaTimeline = (id: string | number, { maxId }: Record<string, any> = {}) =>
-  expandTimeline(`group:${id}:media`, `/api/v1/timelines/group/${id}`, { max_id: maxId, only_media: true, limit: 40, with_muted: true });
+  expandTimeline(`group:${id}:media`, `/api/v1/timelines/group/${id}`, { max_id: maxId, only_media: true, limit: 20, with_muted: true });
 
 const expandHashtagTimeline = (hashtag: string, { url, maxId, tags }: Record<string, any> = {}, done = noOp) => {
   return expandTimeline(`hashtag:${hashtag}`, url || `/api/v1/timelines/tag/${hashtag}`, url ? {} : {
