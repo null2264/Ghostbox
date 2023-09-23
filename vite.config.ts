38db3193a2967243040ff7a3a346966ca97da3e9
ziro <palembani@gmail.com>
2023-09-23 21:18:07 +0700

    chore: Rename index.ejs -> index.html

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   plugins: [
     compileTime(),
     createHtmlPlugin({
-      template: 'app/index.ejs',
+      template: 'app/index.html',
       minify: {
         collapseWhitespace: true,
         removeComments: false,
