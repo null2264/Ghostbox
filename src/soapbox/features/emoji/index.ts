ccb99174ad0eaf7a8cc53ec825b4358208d37f68
ziro <palembani@gmail.com>
2023-10-16 14:17:17 +0700

    fix: Filter out undefined emojis

--- src/soapbox/features/emoji/index.ts
+++ src/soapbox/features/emoji/index.ts
@@ -211,7 +211,7 @@ export default emojify;
 export const buildCustomEmojis = (customEmojis: any) => {
   const emojis: EmojiMart<EmojiMartCustom>[] = [];
 
-  customEmojis.forEach((emoji: any) => {
+  customEmojis.filter((c: any) => typeof (c) !== 'undefined').forEach((emoji: any) => {
     const shortcode = emoji.get('shortcode');
     const url       = emoji.get('static_url');
     const name      = shortcode.replace(':', '');
