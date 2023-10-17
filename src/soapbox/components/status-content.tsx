7d1acbc1c5fedc24a3bdf3388843e9d30af29f01
Ahmad Ansori Palembani <palembani+github@gmail.com>
2023-10-17 02:41:16 +0000

    refactor: Use html-react-parser to render status

--- src/soapbox/components/status-content.tsx
+++ src/soapbox/components/status-content.tsx
@@ -1,18 +1,21 @@
 import clsx from 'clsx';
+import parse, { HTMLReactParserOptions, domToReact, Element as ParserElement } from 'html-react-parser';
 import React, { useState, useRef, useLayoutEffect, useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { useHistory } from 'react-router-dom';
+import { Link as DomLink } from 'react-router-dom';
 
+import { reactText } from 'soapbox/utils/react';
 import { onlyEmoji as isOnlyEmoji } from 'soapbox/utils/rich-content';
 
 import { isRtl } from '../rtl';
 
+import Link from './link';
 import Markup from './markup';
 import Poll from './polls/poll';
 import { Button } from './ui';
 
 import type { Sizes } from 'soapbox/components/ui/text/text';
-import type { Status, Mention } from 'soapbox/types/entities';
+import type { Status } from 'soapbox/types/entities';
 
 const MAX_HEIGHT = 642; // 20px * 32 (+ 2px padding at the top)
 const BIG_EMOJI_LIMIT = 10;
@@ -32,65 +35,11 @@ const StatusContent: React.FC<IStatusContent> = ({
   collapsable = false,
   textSize = 'md',
 }) => {
-  const history = useHistory();
-
   const [isCollapsable, setCollapsable] = useState(false);
   const [collapsed, setCollapsed] = useState(false);
   const [onlyEmoji, setOnlyEmoji] = useState(false);
 
   const node = useRef<HTMLDivElement>(null);
-
-  const onMentionClick = (mention: Mention, e: MouseEvent) => {
-    if (e.button === 0 && !(e.ctrlKey || e.metaKey)) {
-      e.preventDefault();
-      e.stopPropagation();
-      history.push(`/@${mention.acct}`);
-    }
-  };
-
-  const onHashtagClick = (hashtag: string, e: MouseEvent) => {
-    hashtag = hashtag.replace(/^#/, '').toLowerCase();
-
-    if (e.button === 0 && !(e.ctrlKey || e.metaKey)) {
-      e.preventDefault();
-      e.stopPropagation();
-      history.push(`/tags/${hashtag}`);
-    }
-  };
-
-  /** For regular links, just stop propogation */
-  const onLinkClick = (e: MouseEvent) => {
-    e.stopPropagation();
-  };
-
-  const updateStatusLinks = () => {
-    if (!node.current) return;
-
-    const links = node.current.querySelectorAll('a');
-
-    links.forEach(link => {
-      // Skip already processed
-      if (link.classList.contains('status-link')) return;
-
-      // Add attributes
-      link.classList.add('status-link');
-      link.setAttribute('rel', 'nofollow noopener');
-      link.setAttribute('target', '_blank');
-
-      const mention = status.mentions.find(mention => link.href === `${mention.url}`);
-
-      // Add event listeners on mentions and hashtags
-      if (mention) {
-        link.addEventListener('click', onMentionClick.bind(link, mention), false);
-        link.setAttribute('title', mention.acct);
-      } else if (link.textContent?.charAt(0) === '#' || (link.previousSibling?.textContent?.charAt(link.previousSibling.textContent.length - 1) === '#')) {
-        link.addEventListener('click', onHashtagClick.bind(link, link.text), false);
-      } else {
-        link.setAttribute('title', link.href);
-        link.addEventListener('click', onLinkClick.bind(link), false);
-      }
-    });
-  };
 
   const toggleCollapsed: React.MouseEventHandler = (e) => {
     e.stopPropagation();
@@ -124,8 +73,61 @@ const StatusContent: React.FC<IStatusContent> = ({
 
   useLayoutEffect(() => {
     maybeSetOnlyEmoji();
-    updateStatusLinks();
   });
+
+  const options: HTMLReactParserOptions = {
+    replace: (node) => {
+      if (node instanceof ParserElement && ['script', 'iframe'].includes(node.name)) return null;
+
+      if (node instanceof ParserElement && node.name === 'a') {
+        const classes = node.attribs.class?.split(' ') || [];
+
+        if (classes.includes('mention')) {
+          const mention = status.mentions.find(({ url }) => node.attribs.href === url);
+          if (mention)
+            return (
+              <DomLink to={`/@${mention.acct}`} title={`@${mention.acct}`} className='inline-block rounded-full bg-primary-200 px-2 py-1'>
+                @{mention.acct}
+              </DomLink>
+            );
+        }
+
+        if (classes.includes('hashtag') || node.attribs.href.match(/^http(?:s)?:\/\/\S*\/tags/)) {
+          let hashtag: string | undefined = node.attribs.dataTag;
+
+          if (!hashtag) {
+            const child = domToReact(node.children);
+            if (Array.isArray(child)) {
+              // Sometime hashtag is '#<span>tag</span>' for some reason, mostly hashtags from Mastodon
+              hashtag = child.map(e => reactText(e, ['#'])).join('');
+            } else {
+              hashtag = typeof child === 'string' ? child.replace('#', '') : undefined;
+            }
+          }
+
+          if (hashtag)
+            return (
+              <Link to={`/tags/${hashtag}`}>
+                #{hashtag}
+              </Link>
+            );
+        }
+
+        return (
+          // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+          <a
+            {...node.attribs}
+            onClick={(e) => e.stopPropagation()}
+            rel='nofollow noopener'
+            target='_blank'
+            title={node.attribs.href}
+          >
+            {domToReact(node.children, options)}
+          </a>
+        );
+      }
+    },
+  };
 
   const parsedHtml = useMemo((): string => {
     // TODO: Add greentext back?
@@ -140,7 +142,7 @@ const StatusContent: React.FC<IStatusContent> = ({
 
   const baseClassName = 'text-gray-900 dark:text-gray-100 break-words text-ellipsis overflow-x-visible overflow-y-clip relative focus:outline-none';
 
-  const content = { __html: parsedHtml };
+  const content = parse(parsedHtml, options);
   const direction = isRtl(status.search_index) ? 'rtl' : 'ltr';
   const className = clsx(baseClassName, {
     'cursor-pointer': onClick,
@@ -157,10 +159,11 @@ const StatusContent: React.FC<IStatusContent> = ({
       key='content'
       className={className}
       direction={direction}
-      dangerouslySetInnerHTML={content}
       lang={status.language || undefined}
       size={textSize}
-    />,
+    >
+      {content}
+    </Markup>,
   ];
 
   if (isCollapsable && collapsed) {
