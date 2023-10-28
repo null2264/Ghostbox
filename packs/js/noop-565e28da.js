65dffe2f95c1839a4d4ffd4416eda01d63641d82
null2264 <46041660+null2264@users.noreply.github.com>
2023-10-28 06:28:35 +0000

    Deploying to gh-pages from @ null2264/Ghostbox@0127c2fb06bfda4193540592b5e07b99fe53725c 🚀

--- packs/js/noop-565e28da.js
+++
@@ -1,1 +0,0 @@
-import{cE as o}from"../index-1fbe774d.js";function n(){}var t=n;const r=o(t);export{r as n};
--- packs/js/soapbox-config-4e82bc11.js
+++
@@ -1,1 +0,0 @@
-import{bP as a}from"../index-1fbe774d.js";const r={getAds:async o=>{const t=o();return a(t).ads.toArray()}};export{r as default};
