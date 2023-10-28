a92413b5e0f110be8b4e0d542177021eed37132c
null2264 <46041660+null2264@users.noreply.github.com>
2023-10-28 06:54:46 +0000

    Deploying to gh-pages from @ null2264/Ghostbox@28002d1069dd3cef14be1657a9d53533bda4e592 🚀

---
+++ packs/js/index-0727ef4e.js
@@ -0,0 +1,1 @@
+import{a as e,aG as a}from"../index-ba4f4778.js";var r=function(){return e(a,{})};export{r as default};
---
+++ packs/js/noop-151a2208.js
@@ -0,0 +1,1 @@
+import{cE as o}from"../index-ba4f4778.js";function n(){}var t=n;const r=o(t);export{r as n};
